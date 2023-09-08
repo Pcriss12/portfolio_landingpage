377804f3e3a2c294c15a90854c62bed8906cabaf
DESKTOP-Q42QVVA\Paula <pcriss1295@gmail.com>
2023-09-08 15:02:09 -0600

    Change #1

---
+++ astro.config.mjs
@@ -0,0 +1,9 @@
+import image from '@astrojs/image';
+import mdx from '@astrojs/mdx';
+
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [mdx(), image()],
+});
